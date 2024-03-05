c01ac3b485f4be043e7a161ed6de356e6987ede6
m4rs <marcel@marcelclaramunt.cat>
2024-03-05 09:07:40 +0100

    base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
-  base: '/test-pwa', // change if you're deploying to github pages
+  base: '/test-pwa/', // change if you're deploying to github pages
   resolve: {
     preserveSymlinks: true // necessary for yarn link to work
   }
